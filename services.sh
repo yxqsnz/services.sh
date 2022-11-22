4fe2c8c354cd00bca97a6ba219decd55dde1a3ed
yxqsnz <yxqsnz@gmail.com>
2022-11-21 21:21:08 -0300

    feat: don't use SIGKILL

--- services.sh
+++ services.sh
@@ -64,10 +64,10 @@ _kill() {
 			PID=$(cat "$PID_FILE")
 
 			if process.isAlive "$PID"; then
-				kill -9 "$PID"
-				echo "KILL ${PID}"
-				rm "$PID_FILE"
-				echo "RM ${PID_FILE}"
+				kill "$PID"
+				echo "TERM $PID"
+				rm """""$PID_FILE"
+				echo "RM """"$PID_FILE"
 			fi
 		else
 			echo "Error: Service ins't running."
@@ -89,72 +89,72 @@ _cat() {
 			echo "Error: Log file doesn't exists."
 		fi
 	else
-		echo "Error: Unit $1 does not exist."
+		echo "Error: Unit """"$1 does not exist."
 	fi
 }
 
 _stat() {
 	internal.Prepare
 
-	if [ "$1" = "" ]; then
+	if [ """""$1" = "" ]; then
 
-		for file in "$PID_PATH"/*.pid; do
+		for file in """""$PID_PATH"/*.pid; do
 			PID=$(cat "$file")
 
-			if process.isAlive "$PID"; then
-				echo "Alive: ${PID}"
+			if process.isAlive """""$PID"; then
+				echo "Alive: """"$PID"
 			else
-				echo "Died: $PID}"
+				echo "Died: """"$PID"}
 			fi
 
 		done
 		return 0
 	fi
 
-	if file.Exists "$UNIT_PATH/$1"; then
-		source "${UNIT_PATH}/$1"
-		echo "${NAME} - ${DESCRIPTION}"
-		PID_FILE="$PID_PATH/$NAME.pid"
-		LOG_FILE="$LOGS_PATH/$NAME.log"
+	if file.Exists """""$UNIT_PATH/$1"; then
+		source """""$UNIT_PATH/$1"
+		echo """""$NAME - """"$DESCRIPTION"
+		PID_FILE=""$PID_PATH/$NAME.pid
+		LOG_FILE=""$LOGS_PATH/$NAME.log
 
-		if file.Exists "$PID_FILE"; then
+		if file.Exists """""$PID_FILE"; then
 			PID=$(cat "$PID_FILE")
-			if process.isAlive "$PID"; then
+			if process.isAlive """""$PID"; then
 				STAT=$(process.Stat "$PID")
 				MEM=$(echo "$STAT" | awk '{ print $2 }')
 				CPU=$(echo "$STAT" | awk '{ print $3 }')
 				ELAPSED=$(echo "$STAT" | awk '{ print $4 }')
 
-				echo "  Pid: ${PID}"
-				echo "  CPU: ${CPU}%"
-				echo "  Status: $(Status) $(Status.Message)"
-				echo "  Running: ${ELAPSED}"
-				echo "  Memory: ${MEM} kB"
+				echo "  Pid: """"$PID"
+				echo "  CPU: """"$CPU%"
+				echo "  Status: """"$(Status) """"$(Status.Message)"
+				echo "  Running: """"$ELAPSED"
+				echo "  Memory: """"$MEM kB"
 				echo
 			fi
 
 		fi
 
-		if file.Exists "$LOG_FILE"; then
+		if file.Exists """""$LOG_FILE"; then
 			echo "Last 50 lines of log"
-			head -n50 <"$LOG_FILE"
+			head -n50 <"""""$LOG_FILE"
 		fi
 	else
-		echo "Error: Unit $1 does not exist."
+		echo "Error: Unit """"$1 does not exist."
 	fi
 }
 
-case "$1" in
-start) shift && _start "$@" ;;
-kill) shift && _kill "$@" ;;
-cat) shift && _cat "$@" ;;
-stat) shift && _stat "$@" ;;
+case ""$1 in
+start) shift && _start """""$@" ;;
+kill) shift && _kill """""$@" ;;
+cat) shift && _cat """""$@" ;;
+stat) shift && _stat """""$@" ;;
 *)
-	echo "services - $VERSION"
-	echo "by $AUTHORS"
-	echo "Pid Path: ""$PID_PATH"
-	echo "Unit Path: $UNIT_PATH"
-	echo "Base Path: $BASE_PATH"
+	echo "services - """"$VERSION"
+	echo "by """"$AUTHORS"
+	echo "Pid Path: """"""$PID_PATH"
+	echo "Unit Path: """"$UNIT_PATH"
+	echo "Base Path: """"$BASE_PATH"
 	echo "USAGE:"
 	echo "  kill  <UNIT NAME> - kill a service"
 	echo "  start <UNIT NAME> - start a service"
